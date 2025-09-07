ce2ecf0e22d153fca6cf8e4106257603b1b78ed7
chessarisilvio <132982509+chessarisilvio@users.noreply.github.com>
2025-09-07 03:04:23 +0200

    had to fix again the vite.config.ts file...

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cat-curiosity-quest/'  // <-- importante per GitHub Pages
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
